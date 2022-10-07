2b1a7a0c0e50dcb91526bfae927361598997f516
RayBDev <raybernard1337@gmail.com>
2022-10-06 20:46:34 -0500

    Added comments to post resolver and added contingency to comment deletion

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -8,11 +8,14 @@ import { JWTPayloadType } from '../auth/authGen';
 import { Post } from '../models/Post';
 import { Profile } from '../models/Profile';
 
+// @type    Query
 // @desc    Get All posts
 // @access  Public
 const allPosts = async () => {
+  // Find all posts in the database and sort them by date in descending order
   const posts = await Post.find().sort({ date: -1 });
 
+  // If no posts are found throw an error
   if (posts.length < 1) {
     throw new UserInputError('No posts found');
   } else {
@@ -25,11 +28,14 @@ type PostByIdArgs = {
   input: { post_id: Types.ObjectId };
 };
 
+// @type    Query
 // @desc    Get post by id
 // @access  Public
 const postById = async (_: void, args: PostByIdArgs) => {
+  // Search database for post based on post id
   const post = await Post.findById(args.input.post_id);
 
+  // If no post is found throw an error
   if (!post) {
     throw new UserInputError('No post found with that ID');
   } else {
@@ -47,6 +53,7 @@ type CreatePostArgs = {
   };
 };
 
+// @type    Mutation
 // @desc    Create post
 // @access  Private
 const createPost = async (
@@ -54,12 +61,15 @@ const createPost = async (
   args: CreatePostArgs,
   { user }: { user: JWTPayloadType }
 ) => {
+  // Check if user is logged in
   if (!user) throw new AuthenticationError('User not logged in');
 
+  // Validate that the post text length is between 10 and 300 characters
   if (!Validator.isLength(args.input.text, { min: 10, max: 300 })) {
     throw new UserInputError('Post must be between 10 and 300 characters');
   }
 
+  // Create a new post document
   const newPost = new Post({
     text: args.input.text,
     name: args.input.name,
@@ -67,6 +77,7 @@ const createPost = async (
     user: user._id,
   });
 
+  // Save the post document and return new post
   return await newPost.save();
 };
 
@@ -75,6 +86,7 @@ type DeletePostArgs = {
   input: { post_id: Types.ObjectId };
 };
 
+// @type    Mutation
 // @desc    Delete post
 // @access  Private
 const deletePost = async (
@@ -82,13 +94,16 @@ const deletePost = async (
   args: DeletePostArgs,
   { user }: { user: JWTPayloadType }
 ) => {
+  // Check if user is logged in
   if (!user) throw new AuthenticationError('User not logged in');
 
+  // Find user's profile and post to be deted
   const profile = await Profile.findOne({ user: user._id });
   const post = await Post.findById(args.input.post_id);
 
+  // Only allow post deletion if user profile and post exists
   if (post && profile) {
-    // Check for post owner
+    // Check for post owner and throw error if user doesn't own the post
     if (post.user.toString() !== user._id.toString()) {
       throw new AuthenticationError('User not authorized');
     }
@@ -105,6 +120,7 @@ type LikePostArgs = {
   input: { post_id: Types.ObjectId };
 };
 
+// @type    Mutation
 // @desc    Like post
 // @access  Private
 const likePost = async (
@@ -112,12 +128,16 @@ const likePost = async (
   args: LikePostArgs,
   { user }: { user: JWTPayloadType }
 ) => {
+  // Check if user is logged in
   if (!user) throw new AuthenticationError('User not logged in');
 
+  // Find post and user profile
   const profile = await Profile.findOne({ user: user._id });
   const post = await Post.findById(args.input.post_id);
 
+  // Only allow post like if post and profile exists
   if (post && profile && post.likes) {
+    // Check if user has already liked the post by filtering through likes to see if user_id is found
     if (
       post.likes.filter((like) => like.user.toString() === user._id.toString())
         .length > 0
@@ -139,6 +159,7 @@ type UnlikePostArgs = {
   input: { post_id: Types.ObjectId };
 };
 
+// @type    Mutation
 // @desc    Unlike post
 // @access  Private
 const unlikePost = async (
@@ -146,12 +167,16 @@ const unlikePost = async (
   args: UnlikePostArgs,
   { user }: { user: JWTPayloadType }
 ) => {
+  // Check if user is logged in
   if (!user) throw new AuthenticationError('User not logged in');
 
+  // Find post and user profile
   const profile = await Profile.findOne({ user: user._id });
   const post = await Post.findById(args.input.post_id);
 
+  // Only allow unlike if post and profile exists
   if (post && profile && post.likes) {
+    // Check if user has liked the post and if not, throw an error
     if (
       post.likes.filter((like) => like.user.toString() === user._id.toString())
         .length === 0
@@ -159,14 +184,15 @@ const unlikePost = async (
       throw new UserInputError('You have not yet liked this post');
     }
 
-    // Get remove index
+    // Create new array with list of users and find the index of the logged in user
     const removeIndex = post.likes
       .map((item) => item.user.toString())
       .indexOf(user._id.toString());
 
-    // Splice out of array
+    // Splice the user out of the array using the index found above
     post.likes.splice(removeIndex, 1);
 
+    // Return updated post
     return post.save();
   } else {
     throw new UserInputError('Post not found');
@@ -183,6 +209,7 @@ type CommentOnPostArgs = {
   };
 };
 
+// @type    Mutation
 // @desc    Add comment to post
 // @access  Private
 const commentOnPost = async (
@@ -190,17 +217,22 @@ const commentOnPost = async (
   args: CommentOnPostArgs,
   { user }: { user: JWTPayloadType }
 ) => {
+  // Check if user is logged in
   if (!user) throw new AuthenticationError('User not logged in');
 
   let { post_id, text, name, avatar } = args.input;
 
+  // Check if the comment text is between 10 and 300 characters
   if (!Validator.isLength(text, { min: 10, max: 300 })) {
     throw new UserInputError('Post must be between 10 and 300 characters');
   }
 
+  // Find the post that the user wants to comment on
   const post = await Post.findById(post_id);
 
+  // Check if the post exists
   if (post && post.comments) {
+    // Create a new comment object
     const newComment = {
       text,
       name,
@@ -208,10 +240,10 @@ const commentOnPost = async (
       user: user._id,
     };
 
-    // Add to comments array
+    // Add new comment to beginning of comments array
     post.comments.unshift(newComment);
 
-    // Save
+    // Save the post with the new comment
     return post.save();
   } else {
     throw new UserInputError('No post found"');
@@ -223,6 +255,7 @@ type DeleteCommentArgs = {
   input: { post_id: Types.ObjectId; comment_id: Types.ObjectId };
 };
 
+// @type    Mutation
 // @desc    Remove comment from post
 // @access  Private
 const deleteComment = async (
@@ -230,13 +263,17 @@ const deleteComment = async (
   args: DeleteCommentArgs,
   { user }: { user: JWTPayloadType }
 ) => {
+  // Check if user is logged in
   if (!user) throw new AuthenticationError('User not logged in');
 
   let { post_id, comment_id } = args.input;
 
+  // Find the post the user wants to delete their comment from
   const post = await Post.findById(post_id);
 
+  // Check if post exists
   if (post && post.comments) {
+    // Check if the comment exists
     if (
       post.comments.filter(
         (postComment) => postComment._id!.toString() === comment_id.toString()
@@ -245,14 +282,25 @@ const deleteComment = async (
       throw new UserInputError('Comment does not exist');
     }
 
-    // Get remove index
+    if (
+      post.comments.filter(
+        (postComment) =>
+          postComment.user!.toString() === user._id.toString() &&
+          postComment._id!.toString() === comment_id.toString()
+      ).length === 0
+    ) {
+      throw new UserInputError('User can only delete their own comments');
+    }
+
+    // Create a comment id only array an find the index of the comment to be deleted
     const removeIndex = post.comments
       .map((comment) => comment._id!.toString())
       .indexOf(comment_id.toString());
 
-    // Splice comment out of array
+    // Splice the comment found above out of the array
     post.comments.splice(removeIndex, 1);
 
+    // Save the post and return the updated post
     return post.save();
   } else {
     throw new UserInputError('Post or comment not found');
