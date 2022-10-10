ab6f25568450a0a2ec46b614f80a24bc39177eaa
RayBDev <raybernard1337@gmail.com>
2022-10-10 07:12:19 -0500

    Added VSCODE GraphQL configuration file

--- client/graphql.config.js
+++
@@ -1,6 +0,0 @@
-// Used for the GraphQL extension in VSCODE
-// Add your server's graphql endpoint below
-
-module.exports = {
-  schema: 'http://localhost:5000/graphql',
-};
--- graphql.config.js
+++ graphql.config.js
@@ -1,5 +1,13 @@
 // Used for the GraphQL extension in VSCODE
+// Add your schema folder and server's graphql endpoint
 
 module.exports = {
   schema: 'server/src/schema/*.graphql',
+  extensions: {
+    endpoints: {
+      default: {
+        url: 'http://localhost:5000/graphql',
+      },
+    },
+  },
 };
