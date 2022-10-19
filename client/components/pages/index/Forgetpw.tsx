69807ac7103072bb827b33a0eec76e2ac371ded3
RayBDev <raybernard1337@gmail.com>
2022-10-19 07:09:42 -0500

    Added forgetpw component

---
+++ client/components/pages/index/Forgetpw.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Forgetpw = () => {
+  return (
+    <div className="col-span-11 row-span-3 bg-gray-100 rounded-tr-lg rounded-b-lg py-14 px-20">
+      <h2 className="capitalize mb-5">Reset Your Password</h2>
+      <span className="inline-block h-1 w-2 bg-primary rounded-sm mr-1">
+        &#160;
+      </span>
+      <span className="inline-block h-1 w-5 bg-primary rounded-sm">&#160;</span>
+      <h4 className="text-gray-500 my-5 font-medium">
+        Enter your email to get a reset link
+      </h4>
+      FORMIK
+    </div>
+  );
+};
+
+export default Forgetpw;
