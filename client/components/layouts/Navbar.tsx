9c4b5e066b1aec2bda0e01c812a25ac0ba439043
RayBDev <raybernard1337@gmail.com>
2022-10-30 21:06:40 -0500

    Added resonsiveness

--- client/components/layouts/Navbar.tsx
+++ client/components/layouts/Navbar.tsx
@@ -8,7 +8,12 @@ import { AuthContext } from '../../context/authContext';
 import hero from '../../assets/images/background.png';
 import logo from '../../assets/images/logo.png';
 import SearchIcon from '../../assets/icons/search.svg';
+import LogoutIcon from '../../assets/icons/logout.svg';
+import SettingsIcon from '../../assets/icons/settings.svg';
 import ChevronDownIcon from '../../assets/icons/cheveron-down.svg';
+import UsersIcon from '../../assets/icons/group.svg';
+import LoginIcon from '../../assets/icons/login.svg';
+import RegisterIcon from '../../assets/icons/user-plus.svg';
 import styles from './Navbar.module.css';
 import SigninModal from './SigninModal';
 
@@ -17,16 +22,25 @@ const Navbar = () => {
   const [showMobileLinks, setShowMobileLinks] = useState(false);
   const [showSearchDropdown, setShowSearchDropdown] = useState(false);
   const [showSigninModal, setShowSignInModal] = useState(false);
+  const [showUserMenuDropdown, setShowUserMenuDropdown] = useState(false);
   const router = useRouter();
 
   const toggleMobileLinks = () => {
     setShowSearchDropdown(false);
+    setShowUserMenuDropdown(false);
     setShowMobileLinks((prevState) => !prevState);
   };
 
   const toggleSearchDropdown = () => {
     setShowMobileLinks(false);
+    setShowUserMenuDropdown(false);
     setShowSearchDropdown((prevState) => !prevState);
+  };
+
+  const toggleUserMenuDropdown = () => {
+    setShowSearchDropdown(false);
+    setShowMobileLinks(false);
+    setShowUserMenuDropdown((prevState) => !prevState);
   };
 
   const logout = () => {
@@ -42,7 +56,7 @@ const Navbar = () => {
     <>
       {/* Mobile Search Box Dropdown */}
       <div
-        className={`fixed lg:hidden transform-gpu transition-all duration-700 top-16 left-0 w-full shadow-lg bg-white px-3 py-2 ${
+        className={`fixed lg:hidden transform-gpu transition-all duration-700 top-16 sm:top-[5.5rem] left-0 w-full shadow-lg bg-white px-3 py-2 ${
           showSearchDropdown ? 'translate-y-0' : '-translate-y-20'
         }`}
       >
@@ -56,44 +70,53 @@ const Navbar = () => {
 
       {/* Mobile Navigation Menu */}
       <ul
-        className={`transform-gpu transition-all duration-700 fixed top-16 right-0 bg-gray-100 lg:hidden items-center w-full shadow-lg ${
+        className={`transform-gpu transition-all duration-700 fixed top-16 sm:top-24 right-0 bg-gray-100 xl:hidden items-center w-full shadow-lg ${
           showMobileLinks ? 'translate-y-0' : '-translate-y-56'
         }`}
       >
-        <li className={`px-3 pb-3 pt-5 lg:p-2 border-b border-gray-200`}>
-          <Link href="/developers" className="hover:text-secondary">
+        <li className="border-b border-gray-200 px-5 py-3 text-xs">
+          Devlink Menu
+        </li>
+        <li className={`px-5 py-5 flex items-center`}>
+          <UsersIcon className="mr-3 fill-gray-400" />
+          <Link href="/developers" className="hover:text-secondary text-sm">
             Developers
           </Link>
         </li>
         {!state.user._id && (
           <>
-            <li className={`px-3 pb-3 pt-5 lg:p-2 border-b border-gray-200`}>
+            <li className={`px-5 pb-5 flex items-center`}>
+              <LoginIcon className="mr-3 fill-gray-400" />
               <button
-                className="hover:text-secondary"
+                className="hover:text-secondary text-sm"
                 onClick={() => setShowSignInModal(true)}
               >
                 Login
               </button>
             </li>
-            <li className={`p-3 lg:p-2 border-b border-gray-200`}>
-              <button className="hover:text-secondary">Register</button>
+            <li className={`px-5 pb-5 flex items-center`}>
+              <RegisterIcon className="mr-3 fill-gray-400" />
+              <button className="hover:text-secondary text-sm">Register</button>
             </li>
           </>
         )}
         {state.user._id && (
           <>
-            <li className={`p-3 lg:p-2 border-b border-gray-200`}>
+            <li className={`px-5 pb-5 flex items-center`}>
+              <SettingsIcon className="mr-3 fill-gray-400" />
               <Link
                 href={`/${
                   state.user.handle ? state.user.handle : state.user._id
                 }/profile`}
-                className="hover:text-secondary"
               >
-                Profile Settings
+                <button className="hover:text-secondary text-sm">
+                  Profile Settings
+                </button>
               </Link>
             </li>
-            <li className={`p-3 lg:p-2 border-b border-gray-200`}>
-              <button onClick={logout} className="hover:text-secondary">
+            <li className={`px-5 pb-5 flex items-center`}>
+              <LogoutIcon className="mr-3 fill-gray-400" />
+              <button onClick={logout} className="hover:text-secondary text-sm">
                 Logout
               </button>
             </li>
@@ -102,7 +125,7 @@ const Navbar = () => {
       </ul>
 
       {/* Navigation Background */}
-      <div className="fixed h-16 lg:h-[5.5rem] w-screen overflow-hidden">
+      <div className="fixed h-16 sm:h-[5.5rem] w-screen overflow-hidden">
         <Image
           alt="Hero"
           src={hero}
@@ -113,7 +136,11 @@ const Navbar = () => {
       </div>
 
       {/** Main Navigation Bar */}
-      <nav className="fixed w-full py-2 lg:py-5 px-5 lg:px-10 xl:px-20 flex items-center bg-transparent shadow-lg">
+      <nav
+        className={`fixed w-full py-2 sm:py-5 px-5 lg:px-10 xl:px-20 flex items-center bg-transparent ${
+          !showMobileLinks && 'shadow-lg'
+        }`}
+      >
         {/* Logo */}
         <div className="flex items-center mr-28">
           <div className="h-7 w-7 mr-2">
@@ -140,7 +167,12 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu Icon */}
-        <div className="flex items-center xl:hidden ml-auto">
+
+        <div
+          className={`flex items-center ml-auto ${
+            !state.user._id ? 'xl:hidden' : 'lg:hidden'
+          }`}
+        >
           <button
             className="bg-gray-100 rounded-full w-9 h-9 flex justify-center items-center mr-3 hover:fill-primary md:hidden"
             onClick={toggleSearchDropdown}
@@ -163,7 +195,7 @@ const Navbar = () => {
 
         {/* Logged In User Menu */}
         {state.user._id && (
-          <div className="lg:flex items-center ml-auto hidden">
+          <div className="lg:flex items-center ml-auto hidden py-1">
             <Image
               alt={state.user.name}
               src={state.user.avatar}
@@ -178,7 +210,40 @@ const Navbar = () => {
                 {state.user.handle ? state.user.handle : state.user._id}
               </p>
             </div>
-            <ChevronDownIcon className="fill-white" />
+            <button onClick={toggleUserMenuDropdown}>
+              <ChevronDownIcon className="fill-white" />
+            </button>
+            <ul
+              className={`transform-gpu transition-all duration-700 fixed top-[4.5rem] lg:right-10 xl:right-20 bg-gray-100 shadow-lg rounded-md w-64 ${
+                showUserMenuDropdown ? 'opacity-100' : 'opacity-0'
+              }`}
+            >
+              <li className="border-b border-gray-200 px-5 py-2 text-xs">
+                Your Account
+              </li>
+              <li className={`px-5 py-5 flex items-center`}>
+                <SettingsIcon className="mr-3 fill-gray-400" />
+                <Link
+                  href={`/${
+                    state.user.handle ? state.user.handle : state.user._id
+                  }/profile`}
+                  className="hover:text-secondary"
+                >
+                  <button className="text-sm hover:text-secondary">
+                    Profile Settings
+                  </button>
+                </Link>
+              </li>
+              <li className={`px-5 pb-5 flex items-center`}>
+                <LogoutIcon className="mr-3 fill-gray-400" />
+                <button
+                  onClick={logout}
+                  className="hover:text-secondary text-sm"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
           </div>
         )}
 
@@ -193,15 +258,15 @@ const Navbar = () => {
                 Login
               </button>
             </li>
-            <li className="btn btn-ghost">
-              <Link href="/developers" className="">
-                Developers
+            <li>
+              <Link href="/developers">
+                <button className="btn btn-ghost">Developers</button>
               </Link>
             </li>
           </ul>
         )}
       </nav>
-      {showSigninModal && (
+      {showSigninModal && !state.user._id && (
         <SigninModal setShowSignInModal={setShowSignInModal} />
       )}
     </>
