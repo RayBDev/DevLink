6a8fa8ebb84e00b7f0e1cf847d944f945e3087c0
RayBDev <raybernard1337@gmail.com>
2022-11-02 19:53:43 -0500

    Added get posts by user query

--- client/graphql/queries.ts
+++ client/graphql/queries.ts
@@ -59,3 +59,47 @@ export const GET_ALL_POSTS = gql`
     }
   }
 `;
+
+export const GET_POST_BY_ID = gql`
+  query PostById($input: PostIdInput!) {
+    postById(input: $input) {
+      user
+      text
+      name
+      avatar
+      likes {
+        user
+      }
+      comments {
+        user
+        text
+        name
+        avatar
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const GET_POSTS_BY_USER = gql`
+  query AllPosts($input: UserIdInput!) {
+    postsByUser(input: $input) {
+      user
+      text
+      name
+      avatar
+      likes {
+        user
+      }
+      comments {
+        user
+        text
+        name
+        avatar
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
