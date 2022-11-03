68818b7e513a8a9d646ee9ef03d61541b9bd7681
RayBDev <raybernard1337@gmail.com>
2022-11-03 07:06:47 -0500

    Push user to index page if not logged in

--- client/pages/[username]/dashboard.tsx
+++ client/pages/[username]/dashboard.tsx
@@ -1,11 +1,20 @@
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement, useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import Layout from '../../components/layouts/Layout';
 import type { NextPageWithLayout } from '../_app';
 import Header from '../../components/pages/dashboard/Header';
 import Feed from '../../components/pages/dashboard/Feed';
+import { AuthContext } from '../../context/authContext';
 
 const Dashboard: NextPageWithLayout = () => {
+  const { state } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!state.user._id) router.push('/');
+  }, []);
+
   return (
     <section className="container">
       <div className="grid grid-cols-12 gap-10">
